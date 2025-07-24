0e2c84487174ab3312b8e6443bc6c6f8a074f453
zaki764 <143420849+zaki764@users.noreply.github.com>
2025-07-24 13:53:09 +0100

    Add files via upload

---
+++ HoldHandsMod.java
@@ -0,0 +1,10 @@
+package com.yourname.holdhands;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("holdhandsmod")
+public class HoldHandsMod {
+    public HoldHandsMod() {
+        System.out.println("HoldHandsMod loaded!");
+    }
+}
